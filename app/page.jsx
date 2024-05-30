8ca516e3cd0f6aff80185d02acfd525c03002e43
aldinpramudya <aldinarielpramudya@gmail.com>
2024-05-30 11:01:31 +0700

    first-commit

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discover and Share
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">AI-Powered Prompts</span>
+            </h1>
+            <p className="desc text-center">
+                RE:Promptopia is a copy prototype of Prompotopia, an open-source AI Prompt to create and share creative prompt
+            </p>
+        </section>
+    )
+}
+
+export default Home
